4230d5f8e03eeaccf4b5426ce7f74291c60020e2
unknown <patelpreyansh3011@gmail.com>
2022-12-23 01:10:19 +0530

    alignment updated

--- src/components/GeoChart.jsx
+++ src/components/GeoChart.jsx
@@ -2,13 +2,6 @@ import React, { useEffect, useState } from "react";
 import Chart from "react-google-charts";
 import data from "../data/geoData.json";
 
-// export const data = [
-//   ["Country", "Popularity"],
-//   ["US", 400],
-//   ["AT", 790],
-//   ["US", 400],
-// ];
-
 const GeoChart = () => {
   const [convertedData, setConvertedData] = useState([
     ["Country", "Data Usage"],
@@ -24,16 +17,16 @@ const GeoChart = () => {
       array.push(data[i].region);
       array.push(data[i].data);
       setConvertedData((prev) => [...prev, array]);
-    //   array = [];
     }
   };
 
   return (
     <Chart
       chartType="GeoChart"
-      width="100%"
-      height="100vh"
+      width="95%"
+      height="90vh"
       data={convertedData}
+      style={{ margin: "20px" }}
     />
   );
 };
